362d910930a56037b05a83b866edbdb78bd1aac9
Rafael Rendon Pablo <rendon@criticalmasser.com>
2016-02-05 15:40:41 -0600

    Add ShuffleInt64() function

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package arrays
 
+import "github.com/goutil/security"
+
 // ContainsString Returns true if `needle`is contained in `haystack`.
 func ContainsString(haystack []string, needle string) bool {
 	if haystack == nil {
@@ -12,4 +14,16 @@ func ContainsString(haystack []string, needle string) bool {
 		}
 	}
 	return false
+}
+
+// ShuffleInt64 Shuffles array of int64
+func ShuffleInt64(items []int64) {
+	n := int64(len(items))
+	for n > 1 {
+		r := security.RandInt63() % n
+		items[r] ^= items[n-1]
+		items[n-1] ^= items[r]
+		items[r] ^= items[n-1]
+		n--
+	}
 }
